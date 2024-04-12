00c8bbe0a22920b7ae0200ee1143b6efb0beb978
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2024-04-11 21:36:28 -0500

    Update main.py
    creates pink graph

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
 
+# URL to the .npy file
+url = 'https://github.com/Duchstf/quench-detector/raw/signal-analysis/sample-data/Ramp27/ai0.npy'
+
+# Download the .npy file
+response = requests.get(url)
+with open('ai0.npy', 'wb') as file:
+    file.write(response.content)
+
+# Load the .npy file
+data = np.load('ai0.npy')
+
+# Plot the data
+plt.plot(data, color='pink')
+plt.title('Plot of Data from ai0.npy')
+plt.xlabel('Index')
+plt.ylabel('Value')
+plt.show()
